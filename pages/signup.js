a36819070e961465a57180f6fc132c2783cbc10d
Juan Sebastian Arboleda <4lzeck@gmail.com>
2021-03-16 15:04:54 +0000

    add avatar selection to signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,15 @@
 import Head from "next/head";
+import Image from "next/image";
 import React from "react";
-import { Card, Form, Button, FormControl, InputGroup } from "react-bootstrap";
+import {
+  Card,
+  Form,
+  Button,
+  FormControl,
+  InputGroup,
+  Row,
+  Col,
+} from "react-bootstrap";
 import { postData } from "../util/webutils";
 import { Formik } from "formik";
 import * as yup from "yup";
@@ -48,6 +57,7 @@ export default class Signup extends React.Component {
 
   render = () => {
     const schema = yup.object().shape({
+      avatar: yup.number().min(1).max(55),
       username: yup
         .string()
         .required()
@@ -79,19 +89,20 @@ export default class Signup extends React.Component {
             }
           `}
         </style>
-        
+
         <div
-          className="d-flex justify-content-center align-items-center"
-          style={{ height: "80vh" }}
+          className="d-flex vh-100 justify-content-center align-items-center"
+          //style={{ height: "80vh" }}
         >
           <div>
             <Card className="transparent-card">
               <Card.Body>
-                <h3 className="card-title text-center">Sign Up</h3>
+                <h3 className="card-title mt-2 text-center">Sign Up</h3>
                 <Formik
                   validationSchema={schema}
                   onSubmit={this.submit}
                   initialValues={{
+                    avatar: 1,
                     username: "",
                     email: "",
                     password: "",
@@ -108,74 +119,127 @@ export default class Signup extends React.Component {
                     isSubmitting,
                   }) => (
                     <Form
-                      className="px-4 pt-4 pb-2"
-                      style={{ maxWidth: 330 }}
+                      className="px-3 pt-3 pb-2"
                       noValidate
                       onSubmit={handleSubmit}
                     >
-                      <Form.Group controlId="userGroup">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control
-                          type="text"
-                          name="username"
-                          placeholder="Username"
-                          value={values.username}
-                          onChange={handleChange}
-                          isValid={touched.username && !errors.username}
-                          isInvalid={touched.username && !!errors.username}
-                        />
-                        <FormControl.Feedback type="invalid">
-                          {errors.username}
-                        </FormControl.Feedback>
-                      </Form.Group>
-                      <Form.Group controlId="emailGroup">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                          type="email"
-                          name="email"
-                          placeholder="Email"
-                          value={values.email}
-                          onChange={handleChange}
-                          isValid={touched.email && !errors.email}
-                          isInvalid={touched.email && !!errors.email}
-                        />
-                        <FormControl.Feedback type="invalid">
-                          {errors.email}
-                        </FormControl.Feedback>
-                      </Form.Group>
-                      <Form.Group controlId="passwordGroup">
-                        <Form.Label>Password</Form.Label>
-                        <InputGroup>
-                          <FormControl
-                            type={this.state.showPassword ? "text" : "password"}
-                            name="password"
-                            value={values.password}
-                            onChange={handleChange}
-                            isValid={touched.password && !errors.password}
-                            isInvalid={touched.password && !!errors.password}
-                            placeholder="Password"
-                          />
-                          <InputGroup.Append>
-                            <Button
-                              className="border"
-                              variant="light"
-                              onClick={this.changeVisibilityPassword}
+                      <Row>
+                        <Col sm="4">
+                          <div className="d-flex h-100 flex-column justify-content-center align-content-center">
+                            <div
+                              className="mx-auto"
+                              style={{ maxHeight: 110, maxWidth: 110 }}
                             >
-                              {this.state.showPassword ? (
-                                <BsEyeSlashFill />
-                              ) : (
-                                <BsEyeFill />
-                              )}
-                            </Button>
-                          </InputGroup.Append>
-                        </InputGroup>
-                        <FormControl.Feedback
-                          type="invalid"
-                          className={(touched.password && !!errors.password)?"d-block":""}
-                        >
-                          {errors.password}
-                        </FormControl.Feedback>
-                      </Form.Group>
+                              <Image
+                                src={`/assets/img/allenatori/${
+                                  values.avatar || 1
+                                }.png`}
+                                alt="Avatar allenatore"
+                                width={110}
+                                height={110}
+                              />
+                            </div>
+                            <Form.Group
+                              as={Row}
+                              className="mt-3"
+                              controlId="avatarGroup"
+                            >
+                              <Form.Label className="pr-0" column>
+                                Avatar
+                              </Form.Label>
+                              <Col className="pl-0">
+                                <Form.Control
+                                  name="avatar"
+                                  value={values.avatar}
+                                  as="select"
+                                  onChange={handleChange}
+                                >
+                                  {Array.from(new Array(55), (_, i) => (
+                                    <option
+                                      key={i + 1}
+                                      value={i + 1}
+                                      label={i + 1}
+                                    >{i + 1}</option>
+                                  ))}
+                                </Form.Control>
+                              </Col>
+                            </Form.Group>
+                          </div>
+                        </Col>
+                        <Col>
+                          <Form.Group controlId="userGroup">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control
+                              type="text"
+                              name="username"
+                              placeholder="Username"
+                              value={values.username}
+                              onChange={handleChange}
+                              isValid={touched.username && !errors.username}
+                              isInvalid={touched.username && !!errors.username}
+                            />
+                            <FormControl.Feedback type="invalid">
+                              {errors.username}
+                            </FormControl.Feedback>
+                          </Form.Group>
+                          <Form.Group controlId="emailGroup">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                              type="email"
+                              name="email"
+                              placeholder="Email"
+                              value={values.email}
+                              onChange={handleChange}
+                              isValid={touched.email && !errors.email}
+                              isInvalid={touched.email && !!errors.email}
+                            />
+                            <FormControl.Feedback type="invalid">
+                              {errors.email}
+                            </FormControl.Feedback>
+                          </Form.Group>
+                          <Form.Group controlId="passwordGroup">
+                            <Form.Label>Password</Form.Label>
+                            <InputGroup>
+                              <FormControl
+                                type={
+                                  this.state.showPassword ? "text" : "password"
+                                }
+                                name="password"
+                                value={values.password}
+                                onChange={handleChange}
+                                isValid={touched.password && !errors.password}
+                                isInvalid={
+                                  touched.password && !!errors.password
+                                }
+                                placeholder="Password"
+                              />
+                              <InputGroup.Append>
+                                <Button
+                                  className="border"
+                                  variant="light"
+                                  onClick={this.changeVisibilityPassword}
+                                >
+                                  {this.state.showPassword ? (
+                                    <BsEyeSlashFill />
+                                  ) : (
+                                    <BsEyeFill />
+                                  )}
+                                </Button>
+                              </InputGroup.Append>
+                            </InputGroup>
+                            <FormControl.Feedback
+                              type="invalid"
+                              className={
+                                touched.password && !!errors.password
+                                  ? "d-block"
+                                  : ""
+                              }
+                            >
+                              {errors.password}
+                            </FormControl.Feedback>
+                          </Form.Group>
+                        </Col>
+                      </Row>
                       <Button
                         variant="primary"
                         size="lg"
@@ -188,7 +252,7 @@ export default class Signup extends React.Component {
                     </Form>
                   )}
                 </Formik>
-                <p className="px-4 pb-2 pt-3" style={{ maxWidth: 330 }}>
+                <p className="px-3 mb-2 pt-3">
                   Hai già un account? Accedi cliccando
                   <a href="/login"> qui!</a>
                 </p>
